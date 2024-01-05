86d7188e3c56c0b5d1725f018c084da08a8ceff2
gabriel <soutogabriel04@gmail.com>
2024-01-05 19:44:20 -0300

    feat: getting names and values of activities

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,5 +11,21 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    int activities = Integer.parseInt(sc.nextLine());
+
+    for (int i = 1; i <= activities; i++) {
+      System.out.println("Digite o nome da atividade " + i + ":");
+
+      String activityName = sc.nextLine();
+      System.out.println("Digite o peso da atividade " + i + ":");
+
+      String valueActivity = sc.nextLine();
+
+
+    }
+
   }
 }
