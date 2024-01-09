1642fe2f1aee08d7579d100d9abfbe00332ee744
gabriel <soutogabriel04@gmail.com>
2024-01-09 17:09:56 -0300

    refactor: all pt sentences to english

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,4 +1,4 @@
-package com.trybe.java.regraprogressao;
+package com.trybe.java.progressionrule;
 
 import java.util.Scanner;
 
@@ -8,25 +8,25 @@ import java.util.Scanner;
 public class App {
 
   /**
-   * Metodo main.
+   * Main method.
    */
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
-    System.out.println("Digite a quantidade de atividades para cadastrar: ");
+    System.out.println("Enter the number of activities to register: ");
     int activities = Integer.parseInt(sc.nextLine());
 
     int sum = 0;
     int valueGrades = 0;
 
     for (int i = 1; i <= activities; i++) {
-      System.out.println("Digite o nome da atividade " + i + ":");
+      System.out.println("Enter the name of activity " + i + ":");
       String activityName = sc.nextLine();
 
-      System.out.println("Digite o peso da atividade " + i + ":");
+      System.out.println("Enter the weight of activity " + i + ":");
       int valueActivity = Integer.parseInt(sc.nextLine());
 
-      System.out.println("Digite a nota obtida para " + activityName + ":");
+      System.out.println("Enter the grade obtained for " + activityName + ":");
       int gradeOfActivity = Integer.parseInt(sc.nextLine());
 
       sum += valueActivity;
@@ -34,21 +34,20 @@ public class App {
       valueGrades += valueActivity * gradeOfActivity;
     }
 
-    // Verifica se a soma dos pesos é 100
     if (sum != 100) {
-      System.out.println("A soma dos pesos é diferente de 100!");
-      return; // Encerra o programa se a soma dos pesos não for 100
+      System.out.println("The sum of weights is different from 100!");
+      return;
     }
 
-    double result = (double) valueGrades / sum; // Calcula a média ponderada
+    double result = (double) valueGrades / sum;
 
     if (result >= 85) {
-      System.out.println("Parabéns! Você alcançou " + result + "%"
-          + " E temos o prazer de informar que você obteve aprovação!");
+      System.out.println("Congratulations! You achieved " + result + "%"
+          + " and we are pleased to inform you that you have been approved!");
     } else {
       System.out.println(
-          "Lamentamos informar que, com base na sua pontuação alcançada neste período, " + result
-              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+          "We regret to inform you that, based on your score achieved in this period, " + result
+              + "%, you did not reach the minimum score required for your approval.");
     }
   }
 }
