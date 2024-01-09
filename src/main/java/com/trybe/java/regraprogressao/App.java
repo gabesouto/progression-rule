b5030244e29d534c9957e5471a47db1d06e0d045
gabriel <soutogabriel04@gmail.com>
2024-01-09 16:45:44 -0300

    feat: getting final result

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -17,27 +17,38 @@ public class App {
     int activities = Integer.parseInt(sc.nextLine());
 
     int sum = 0;
+    int valueGrades = 0;
 
     for (int i = 1; i <= activities; i++) {
       System.out.println("Digite o nome da atividade " + i + ":");
-
       String activityName = sc.nextLine();
+
       System.out.println("Digite o peso da atividade " + i + ":");
+      int valueActivity = Integer.parseInt(sc.nextLine());
 
-      String valueActivity = sc.nextLine();
-      System.out.println(
-          "Digite a nota obtida para " + activityName + ":");
-
+      System.out.println("Digite a nota obtida para " + activityName + ":");
       int gradeOfActivity = Integer.parseInt(sc.nextLine());
 
-      int checkSum = sum += Integer.parseInt(valueActivity);
+      sum += valueActivity;
 
-      if (checkSum != 100) {
-        System.out.println("A soma dos pesos é diferente de 100!");
-      }
-
-
+      valueGrades += valueActivity * gradeOfActivity;
     }
 
+    // Verifica se a soma dos pesos é 100
+    if (sum != 100) {
+      System.out.println("A soma dos pesos é diferente de 100!");
+      return; // Encerra o programa se a soma dos pesos não for 100
+    }
+
+    double result = (double) valueGrades / sum; // Calcula a média ponderada
+
+    if (result >= 85) {
+      System.out.println("Parabéns! Você alcançou " + result + "%"
+          + " E temos o prazer de informar que você obteve aprovação!");
+    } else {
+      System.out.println(
+          "Lamentamos informar que, com base na sua pontuação alcançada neste período, " + result
+              + "%, você não atingiu a pontuação mínima necessária para sua aprovação.");
+    }
   }
 }
